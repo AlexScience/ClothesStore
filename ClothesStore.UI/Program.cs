bf2f8c87f7149280f6c839895756047cef927500
AlexScience <57524619+AlexScience@users.noreply.github.com>
2023-07-01 17:31:41 +0300

    Add Table

--- ClothesStore.UI/Program.cs
+++ ClothesStore.UI/Program.cs
@@ -1,10 +1,32 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Blazorise.Modules;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+
 using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using ClothesStore.Models.Models;
+using ClothesStore.UI.Pages;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services
+    .AddBlazorise( options =>
+    {
+        options.Immediate = true;
+    } )
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
+
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IEntity, Client>();
+builder.Services.AddScoped<ClothesStore.UI.Pages.Clients>();
 
 var app = builder.Build();
 
@@ -22,6 +44,8 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+
 
 
 app.Run();
